6bacb056d6be411584c4f31b1e99278194928c25
Julen Bohoyo <73544256+julenbhy@users.noreply.github.com>
2024-05-21 12:40:10 +0200

    Update models_config.py

--- models_config.py
+++ models_config.py
@@ -3,6 +3,7 @@ from tensorflow.keras.applications.resnet_v2 import ResNet50V2, preprocess_input
 from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input as vgg16_preprocess_input
 from tensorflow.keras.applications.inception_v3 import InceptionV3, preprocess_input as inception_v3_preprocess_input
 
+# Define dictionary for model loading
 MODELS = {
     'mobilenet_v2': (MobileNetV2, mobilenet_v2_preprocess_input),
     'resnet50_v2': (ResNet50V2, resnet50_v2_preprocess_input),
